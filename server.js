3f2fe78baec44ec73a2fd55afa9458334f824a99
Gary <gary.velasquez.lopez@gmail.com>
2017-12-06 14:18:33 -0300

    updated functions structure

--- server.js
+++ server.js
@@ -1,55 +1,80 @@
 const express = require('express')
 const app = express()
 const request = require('request')
-const Browser = require('zombie')
 const WooCommerceAPI = require('woocommerce-api')
-
-var async = require('async');
-
-//zones= 1530 -3110
-//methods 1616 - 3204
+const async = require('async')
 
 var dataObj = {
   settings: {
     'tax_status': 'none',
     'weight_type': 'greater',
     'price_type': 'excl',
-    'calculation_type': 'line'
+    'calculation_type': 'line',
+    'title' : 'Costo despacho'
   }
 };
 
-
 var WooCommerce = new WooCommerceAPI({
   url: 'http://52.201.187.33',
-  consumerKey: 'ck_c9501002eeb2bfbd04be7596cd0fc8893bad38f1 ',
-  consumerSecret: 'cs_0bd7e5cf6b513bce52a733320fca69aef74c5d76',
+  consumerKey: 'ck_',
+  consumerSecret: 'cs_',
   wpAPI: true,
   version: 'wc/v2'
 });
 
-
-function getShippingZones() {
-
+function removeShippingZones() {
+  shippingArrayID = [];
   WooCommerce.get('shipping/zones', function(err, data, res) {
     var shippingArrayID = []
     var obj = JSON.parse(res);
     for (var key in obj) {
       shippingArrayID.push(obj[key]["id"]);
     }
-
-    console.log(shippingArrayID);
-
     //async.eachSeries(shippingArrayID, function(data, callback) {
     //  WooCommerce.delete('shipping/zones/' + data + '?force=true', function(err, data, res) {
     //    console.log(res);
     //    callback(null);
     //  });
     //});
-
   });
-
 }
 
+function updateEveryMethod() {
+  shippingArrayID = [];
+
+  WooCommerce.get('shipping/zones', function(err, data, res) {
+    var obj = JSON.parse(res);
+    for (var key in obj) {
+      shippingArrayID.push(obj[key]["id"]);
+    }
+    //console.log(shippingArrayID);
+    //shippingArrayID = ['1531'];
+    async.eachSeries(shippingArrayID, function(data, callback) {
+      methodArrayID = [];
+      WooCommerce.get('shipping/zones/' + data + '/methods', function(err, datar, res) {
+        var obj = JSON.parse(res);
+        for (var key in obj) {
+          methodArrayID.push(obj[key]["id"]);
+        }
+        async.eachSeries(methodArrayID, function(inner, callback) {
+          WooCommerce.put('shipping/zones/' + data + '/methods/' + inner, dataObj, function(err, data, res) {
+            console.log("updated!");
+            callback(null);
+          });
+        });
+        callback(null);
+      });
+    }, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('everything ok!');
+    });
+  });
+}
+
+//updateEveryMethod();
+//removeShippingZones();
 
 
 //WooCommerce.get('shipping/zones/21/methods/27', function(err, data, res) {
@@ -63,65 +88,3 @@ function getShippingZones() {
 //WooCommerce.delete('shipping/zones/21?force=true', function(err, data, res) {
 //  console.log(res);
 //});
-
-
-
-
-function updateEveryMethod() {
-
-  shippingArrayID = [];
-
-
-  WooCommerce.get('shipping/zones', function(err, data, res) {
-    var obj = JSON.parse(res);
-    for (var key in obj) {
-      shippingArrayID.push(obj[key]["id"]);
-    }
-
-    //console.log(shippingArrayID);
-    shippingArrayID = ['1531'];
-
-
-    async.eachSeries(shippingArrayID, function(data, callback) {
-      methodArrayID = [];
-      WooCommerce.get('shipping/zones/' + data + '/methods', function(err, data2, res) {
-        var obj = JSON.parse(res);
-        for (var key in obj) {
-          methodArrayID.push(obj[key]["id"]);
-        }
-
-        console.log("============");
-        console.log(data);
-
-        //here =============================
-        async.eachSeries(methodArrayID, function(inner, callback) {
-          WooCommerce.put('shipping/zones/' + data + '/methods/' + inner, dataObj, function(err, data, res) {
-            console.log(res);
-            console.log("updated!");
-            callback(null);
-          });
-
-
-
-
-        });
-        //=================================
-
-
-        callback(null);
-      });
-    }, function(err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log('ok');
-    });
-
-
-
-
-  });
-
-}
-
-updateEveryMethod();
